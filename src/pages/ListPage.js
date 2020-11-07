4d79dd4cbeef1d8280f183667cbb0a13f369aaf1
Woo-yeong Kim <kimwy1997@gmail.com>
2020-11-07 21:01:13 +0900

    리스트 뷰 셋업

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, withRouter } from "react-router-dom";
+import Footer from "../components/common/Footer";
 import Header from "../components/common/Header";
 import ListAllComponent from "../components/list/ListAllComponent";
 import ListMyComponent from "../components/list/ListMyComponent";
@@ -10,6 +11,7 @@ export default withRouter(({ location: { pathname } }) => {
       <Header />
       <Route path={"/list/my-sky"} component={ListMyComponent} />
       <Route path={"/list"} exact component={ListAllComponent} />
+      <Footer />
     </>
   );
 });
