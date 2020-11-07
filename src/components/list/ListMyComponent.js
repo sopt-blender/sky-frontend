f346775d3b402504c25180879ad2d94c8db87c38
Woo-yeong Kim <kimwy1997@gmail.com>
2020-11-07 21:01:51 +0900

    내가 본 하늘 프로토타입

--- src/components/list/ListMyComponent.js
+++ src/components/list/ListMyComponent.js
@@ -1,7 +1,42 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0 14rem;
+  display: flex;
+  height: 45.5rem;
+`;
+
+const Image = styled.img`
+  background-color: red;
+  color: blue;
+  margin-right: 11.6rem;
+`;
+
+const Options = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 8.7rem;
+`;
+
+const OptionElem = styled.span`
+  font-family: inherit;
+  font-weight: bold;
+  text-align: left;
+  font-size: 3rem;
+`;
 
 const ListMyComponent = () => {
-  return <div>List mine</div>;
+  return (
+    <Container>
+      <Image src="http://placekitten.com/810/455" />
+      <Options>
+        <OptionElem>최신 순 정렬</OptionElem>
+        <OptionElem>승인 여부</OptionElem>
+        <OptionElem>좋아요</OptionElem>
+      </Options>
+    </Container>
+  );
 };
 
 export default ListMyComponent;
