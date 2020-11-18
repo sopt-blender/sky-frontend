5ee17e28553b1704f636a4250e9820f80b970e23
junholee <juno7803@naver.com>
2020-11-19 02:56:36 +0900

    [ADD] 잠시 멈춰 바라보기 버튼 기능 구현 완료

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,16 +1,11 @@
-import React, { useRef,useState } from "react";
+import React, { useRef,useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Navigation, Autoplay } from "swiper";
 import "swiper/swiper-bundle.css";
-import likeIcon from "../../assets/icons/like_active.svg";
 import styled, {css} from "styled-components";
 import {StyledButtonShadow} from "../common/ButtonShadowComponent";
-
-const merong = (e) => {
-  e.preventDefault()
-  e.stopPropagation()
-
-}
+import likeIcon from "../../assets/icons/like_active.svg";
+import btnRight from "../../assets/icons/btn_right.svg";
 
 const sizes = {
   desktop:102.4,
@@ -40,7 +35,11 @@ const Header = styled.div`
   z-index: 2;
   ${media.desktop`justify-content: space-between;`}
   ${media.tablet`justify-content: space-around; margin-left: 4rem;`}
-  ${media.phone`justify-content: center;`}
+  ${media.phone`
+    justify-content: center;
+    padding: 0;
+    margin-left: 0;
+  `}
 `
 
 const Desc = styled.div`
@@ -50,8 +49,10 @@ const Desc = styled.div`
 `;
 
 const Img = styled.img`
-  // ${media.phone`
-  // `}
+   ${media.phone`
+      position: absolute;
+      right: 8px;
+   `}
 `;
 
 const PrevBtn = styled.button.attrs((props) => ({
@@ -59,8 +60,8 @@ const PrevBtn = styled.button.attrs((props) => ({
 }))`
   position: absolute;
   border: none;
-  width: 7rem;
-  height: 7rem;
+  width: 5rem;
+  height: 5rem;
   padding: 0;
   font-size: 10.7rem;
   border-radius: 50%;
@@ -75,6 +76,10 @@ const PrevBtn = styled.button.attrs((props) => ({
     outline:none;
     box-shadow:none;
   }
+  ${media.phone`
+    width: 5rem;
+    height: 5rem;
+  `}
 `;
 
 const NextBtn = styled.button.attrs((props) => ({
@@ -82,21 +87,24 @@ const NextBtn = styled.button.attrs((props) => ({
 }))`
   position:absolute;
   border: none;
-  width: 7rem;
-  height: 7rem;
+  width: 5rem;
+  height: 5rem;
   padding: 0;
   border-radius: 50%;
   box-shadow: 0 0 40px 0 rgba(0, 0, 0, 0.1);
-  background-color: #ffffff;
+  /* background-color: #ffffff; */
   margin-right: 11rem;
-  color: #000000;
-  font-weight: bold;
+  /* color: #000000; */
+  /* font-weight: bold; */
+  background-image: url(${btnRight});
+  background-repeat: no-repeat; 
   ${media.desktop`margin-right:8rem;`}
   ${media.tablet`margin-right:5rem;`}
   &:focus{
     outline:none;
     box-shadow:none;
   }
+  ${media.phone``}
 `;
 
 const StopBtnWrapper = styled.div`
@@ -104,7 +112,9 @@ const StopBtnWrapper = styled.div`
   justify-content: center;
 `;
 
-const StopBtn = styled(StyledButtonShadow)`
+const StopBtn = styled(StyledButtonShadow).attrs((props)=>({
+  className: "swiper-container"
+}))`
   width: 38.4rem;
   height: 7.5rem;
   padding: 2.4rem 7.2rem;  
@@ -129,13 +139,21 @@ SwiperCore.use([Navigation, Autoplay]);
 function Main({posts, onSlideChange}) {
   const prevRef = useRef(null);
   const nextRef = useRef(null);
-  const [bool,setBool] = useState(true);
+  const swiperRef = useRef(); // swiper DOM에 접근하여 swiper 객체에 접근하기 위함(swiper.autoplay.start() 호출 위함)
+  const [autoPlay,setAutoPlay] = useState(true);
 
+  useEffect(()=>{
+    if(autoPlay){
+      swiperRef.current.swiper.autoplay.start();
+    }else{
+      swiperRef.current.swiper.autoplay.stop();
+    }
+  },[autoPlay]);
+  
   return (
     <>
       <Swiper 
-        navigation
-        ={{
+        navigation = {{
           prevEl: prevRef.current ? prevRef.current : null,
           nextEl: nextRef.currnet ? nextRef.current : null,
         }}
@@ -145,7 +163,8 @@ function Main({posts, onSlideChange}) {
           swiper.navigation.update();
         }}
         onSlideChange={(swiper)=> onSlideChange(swiper.activeIndex)}
-        autoplay={bool}
+        autoplay={autoPlay}
+        ref={swiperRef}
       >
         {posts.map((post,index) =>
           <SwiperSlide key={`slide-${index}`} style={swiperStyle}>
@@ -156,10 +175,10 @@ function Main({posts, onSlideChange}) {
             </Header>
           </SwiperSlide>
         )}
-        <PrevBtn ref={prevRef} onMouseDown={merong}/>
+        <PrevBtn ref={prevRef}/>
         <NextBtn ref={nextRef}/>
         <StopBtnWrapper>
-          <StopBtn onClick={()=> setBool(!bool)}>잠시 멈춰 바라보기</StopBtn>
+          <StopBtn onClick={()=>setAutoPlay(!autoPlay)}>잠시 멈춰 바라보기</StopBtn>
         </StopBtnWrapper>
       </Swiper>
     </>
